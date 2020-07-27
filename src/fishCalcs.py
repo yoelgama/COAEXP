b41d619bb265714fde9aa2cd995b1d3718610e88
Joel Gama <joeldson1997@gmail.com>
2020-07-27 18:56:37 -0300

    calcula a quantidade de baits para cada nível da tabela

--- src/fishCalcs.py
+++ src/fishCalcs.py
@@ -1,3 +1,45 @@
 import pandas as pd
 
-fishFrame = pd.read_csv()
+fishFrame = pd.read_csv("dataframes/fishingFrame.csv")
+expFrame = pd.read_csv("dataframes/experienceFrame.csv")
+
+expFrame["Total Experience"] = expFrame["Total Experience"].str.replace(',', '').astype(int)
+
+fish = {}
+
+expList = list(expFrame["Total Experience"].unique())
+
+levellist = list(fishFrame['Fishing-Level'])
+fishlist = list(fishFrame['Fish'])
+baitlist = list(fishFrame['Bait used'])
+fishexplist = list(fishFrame['Fishing exp'])
+
+max = len(levellist)
+count = 1
+
+print(levellist)
+print(expList)
+expDict = {}
+for item in expList:
+    expDict[count] = item
+    count += 1
+
+print(expDict)
+count = 0
+while count < max:
+    fish[levellist[count]] = {
+        'fish': fishlist[count],
+        'bait': baitlist[count],
+        'exp given': int(fishexplist[count].replace(',', '')),
+
+    }
+    if count + 1 < len(levellist):
+        fish[levellist[count]]['next'] = levellist[count + 1]
+        fish[levellist[count]]['exp to next'] = expDict[fish[levellist[count]]['next']] - expDict[levellist[count]]
+        fish[levellist[count]]['baits to next'] = fish[levellist[count]]['exp to next'] // \
+                                                  fish[levellist[count]].get('exp given')
+
+    count += 1
+
+dataframe = pd.DataFrame(fish)
+dataframe.to_csv("dataframes/baitsNeeded.csv", encoding='utf-8')
