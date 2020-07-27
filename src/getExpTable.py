9208ae47f98c6c221832a7c3ebbd7e06fba51b9b
Joel Gama <joeldson1997@gmail.com>
2020-07-27 00:26:09 -0300

    Tabela wood, xp e mining prontas

--- src/getExpTable.py
+++ src/getExpTable.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 
 def create_chunks(list_name, n):
@@ -11,7 +12,7 @@ def create_chunks(list_name, n):
     return chunkList
 
 
-def get_csv(url, filename):
+def get_csv(url, filename, strIMP="None"):
     page = requests.get(url)
 
     if page.status_code != 200:
@@ -20,31 +21,39 @@ def get_csv(url, filename):
     soup = BeautifulSoup(page.content, 'html.parser')
 
     treeTB = soup.find('table', class_="wikitable")
+    if strIMP != "None":
+        tr = treeTB.findNext('tr')
+        if strIMP in tr:
+            print("ok!")
+        else:
+            treeTB = soup.select('table:contains(' + strIMP + ')')[0]
 
+    print(treeTB)
     dictTree = {}
-    listTree = list(treeTB.text.strip().split("\n"))
     thList = []
     tdList = []
 
-    for thElement in treeTB.findAll('th'):
-        for th in thElement:
-            dictTree[th.strip()] = []
-            thList.append(th.strip())
+    for th in treeTB.findAll('th'):
+        dictTree[th.text.strip()] = []
+        thList.append(th.text.strip())
 
     for td in treeTB.findAll('td'):
         tdList.append(td.text.strip())
 
     tdList = create_chunks(tdList, len(dictTree))
+    tdList = sorted(tdList, key=lambda x: int(x[0]))
+    print(tdList)
 
     for lista in tdList:
         count = 0
         for elemento in lista:
             dictTree[thList[count]].append(elemento)
             count += 1
-
+    # dictTree = sorted(dictTree)
     treeframe = pd.DataFrame(dictTree)
-
     treeframe.to_csv(filename, index=False, encoding='utf-8')
 
 
 get_csv("https://www.curseofaros.wiki/wiki/Trees", "treeFrame.csv")
+get_csv("https://www.curseofaros.wiki/wiki/XP", "xpFrame.csv")
+get_csv("https://www.curseofaros.wiki/wiki/Mining", "miningFrame.csv", "Ores")
